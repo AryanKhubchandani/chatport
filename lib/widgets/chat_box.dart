dd118c5734105e2f7476ae7d29ad98d135be30c5
Aryan <aryan.khubchandani@gmail.com>
2021-12-10 04:29:10 +0530

    chore: add chatbox widget

---
+++ lib/widgets/chat_box.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class ChatBox extends StatefulWidget {
+  String name;
+  String message;
+  String image;
+  String time;
+  bool online;
+  bool isMessageRead;
+  ChatBox(
+      {required this.name,
+      required this.message,
+      required this.image,
+      required this.time,
+      required this.online,
+      required this.isMessageRead});
+  @override
+  _ConversationListState createState() => _ConversationListState();
+}
+
+class _ConversationListState extends State<ChatBox> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding:
+            const EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(widget.image),
+                        maxRadius: 30,
+                      ),
+                      if (widget.online == true)
+                        Container(
+                          height: 15,
+                          width: 16,
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2.0,
+                              )),
+                        ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Container(
+                        color: Colors.transparent,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.name,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              widget.message,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.grey.shade600,
+                                  fontWeight: widget.isMessageRead
+                                      ? FontWeight.bold
+                                      : FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              widget.time,
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: widget.isMessageRead
+                      ? FontWeight.bold
+                      : FontWeight.normal),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
