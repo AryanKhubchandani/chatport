b437e3ea69532934bfa05230b0e779837acee64b
Aryan <aryan.khubchandani@gmail.com>
2021-12-10 04:30:08 +0530

    feat: add chat page UI

--- lib/screens/chatpage.dart
+++ lib/screens/chatpage.dart
@@ -1,3 +1,4 @@
+import 'package:chatport/widgets/chat_box.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -10,7 +11,74 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(child: Text("Chat")),
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Chats",
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.person_add,
+                        ),
+                        mini: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search...",
+                    hintStyle: TextStyle(color: Colors.grey[600]),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey[600],
+                      size: 20,
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    contentPadding: const EdgeInsets.all(8.0),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+              ListView.builder(
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(top: 15),
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, index) => ChatBox(
+                      name: "Aryan",
+                      message:
+                          "Hey,f dksfj;dalsk fjldksa jaf;ldskj flkdsaj f;ol fkdsj ;fdklsj",
+                      image: "image",
+                      time: "5",
+                      online: true,
+                      isMessageRead: false))
+            ],
+          ),
+        ),
       ),
     );
   }
