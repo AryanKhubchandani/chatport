2226a901fda04f3fed37b2fc14b84c80f8b3db0f
Aryan <aryan.khubchandani@gmail.com>
2021-12-09 13:05:02 +0530

    feat: add bottom navigation bar

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,22 +1,33 @@
-// ignore_for_file: file_names
-
 import 'package:chatport/screens/loginpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int _page = 1;
+  late PageController _c;
+
+  @override
+  void initState() {
+    super.initState();
+    _c = PageController(
+      keepPage: true,
+      initialPage: _page,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: const Text("Home Page"),
         actions: [
           IconButton(
               onPressed: () {
@@ -27,6 +38,55 @@ class _HomePageState extends State<HomePage> {
               icon: const Icon(Icons.logout))
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        unselectedIconTheme: IconThemeData(color: Colors.grey[400]),
+        selectedIconTheme: IconThemeData(color: _getColor()),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        currentIndex: _page,
+        selectedItemColor: _getColor(),
+        onTap: (index) {
+          _c.animateToPage(index,
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeInOutSine);
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _c,
+        onPageChanged: (newPage) {
+          setState(() {
+            _page = newPage;
+          });
+        },
+        children: <Widget>[],
+      ),
     );
+  }
+
+  Color _getColor() {
+    switch (_page) {
+      case 0:
+        return Colors.blue;
+      case 1:
+        return Colors.blue;
+      case 2:
+        return Colors.black;
+      default:
+        return Colors.blue;
+    }
   }
 }
